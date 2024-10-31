8395865593997195c4d83909136b9c3422d7de12
Daniel Crimp <daniel.crimp@outlook.com>
2024-10-31 14:20:50 +0000

    stubs

---
+++ src/stores/store.py
@@ -0,0 +1,16 @@
+
+
+class Store:
+
+    def __init__(self):
+        self.client = self._init_client()
+        self.cart = self._init_cart()
+
+    def _init_client(self):
+        pass
+
+    def _init_cart(self):
+        pass
+
+    def search_products(self, query):
+        pass
